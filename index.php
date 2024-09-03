ced6e78ef41eeb5c3614ad768b25cd17428c724d
Oleksandra Ubska <alessandraubska@gmail.com>
2024-09-03 15:47:36 +0200

    feat: print movies in page

--- index.php
+++ index.php
@@ -17,6 +17,19 @@ require_once __DIR__ . '/Data/db.php';
 
 <body>
 
+    <h3>Movie list:</h3>
+    <ul>
+        <?php foreach ($movies as $movie): ?>
+            <li>
+                <h4>Title: <?php echo $movie->name; ?></h4>
+                <h4>Director: <?php echo $movie->director; ?></h4>
+                <h4>Year: <?php echo $movie->year; ?></h4>
+                <h4>Genres: <?php echo $movie->name; ?></h4>
+                <h4>Duration: <?php echo $movie->name; ?>min</h4>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
 </body>
 
 </html>
